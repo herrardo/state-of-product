d479f628872b200217851c1611824f471e4a5a57
herrardo <gerardov@empathy.co>
2022-12-11 15:59:24 +0100

    build: add gh pages github workflow

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,6 @@ export default {
     title: 'State Of Product',
     description: 'This is a list of Product Management resources',
     lang: 'en-US',
-    outDir: '../public',
     themeConfig: {
         sidebar: [
             {
@@ -19,5 +18,6 @@ export default {
                 ]
             }
         ]
-    }
+    },
+    base: '/state-of-product/'
 }
